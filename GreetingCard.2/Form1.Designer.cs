5935ee993031ae1b639c2eb514f65dd5a6e09aaf
ChrisJR3 <chrirous315@ed.amdsb.ca>
2018-09-20 14:58:31 -0400

    Hello Again

--- GreetingCard.2/Form1.Designer.cs
+++ GreetingCard.2/Form1.Designer.cs
@@ -29,17 +29,39 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(335, 243);
+            this.button1.Location = new System.Drawing.Point(463, 253);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Yes!";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(571, 253);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Yes!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(327, 92);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Play Again?";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Background
             // 
@@ -49,6 +71,8 @@
             this.BackgroundImage = global::GreetingCard._2.Properties.Resources.Back_of_Card;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(740, 513);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Background";
             this.Text = "Form1";
@@ -60,6 +84,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- GreetingCard.2/Form1.cs
+++ GreetingCard.2/Form1.cs
@@ -10,7 +10,11 @@ using System.Media;
 using System.Windows.Forms;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/// <summary>
+/// Created By: Chris Rouse
+/// Date: Sept 2018
+/// Description: A card trick.
+/// </summary>
 namespace GreetingCard._2
 {
     public partial class Background : Form
@@ -18,22 +22,13 @@ namespace GreetingCard._2
         public Background()
         {
             InitializeComponent();
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
         }
 
         private void Background_Load(object sender, EventArgs e)
         {
-         // Come back to this
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click_1(object sender, EventArgs e)
-        {
-            button1.Visible = false;
-
             Graphics g = this.CreateGraphics();
             Pen drawPen = new Pen(Color.Black, 10);
 
@@ -49,25 +44,81 @@ namespace GreetingCard._2
             Font Ariel = new Font("Ariel", 16, FontStyle.Bold);
             Font Cards = new Font("Comfortaa", 16, FontStyle.Bold);
 
+            //For diamonds
+
+            Point[] diamond1 = new Point[4];
+            diamond1[0] = new Point(65, 70);
+            diamond1[1] = new Point(85, 90);
+            diamond1[2] = new Point(65, 110);
+            diamond1[3] = new Point(45, 90);
+
+            Point[] diamond2 = new Point[4];
+            diamond1[0] = new Point(165, 70);
+            diamond1[1] = new Point(185, 90);
+            diamond1[2] = new Point(165, 110);
+            diamond1[3] = new Point(145, 90);
+
+            Point[] diamond3 = new Point[4];
+            diamond1[0] = new Point(265, 70);
+            diamond1[1] = new Point(285, 90);
+            diamond1[2] = new Point(265, 110);
+            diamond1[3] = new Point(245, 90);
+
+            Point[] diamond4 = new Point[4];
+            diamond1[0] = new Point(365, 70);
+            diamond1[1] = new Point(385, 90);
+            diamond1[2] = new Point(365, 110);
+            diamond1[3] = new Point(345, 90);
+
+            //Lower Half
+
+            Point[] diamond5 = new Point[4];
+            diamond1[0] = new Point(65, 340);
+            diamond1[1] = new Point(85, 360);
+            diamond1[2] = new Point(65, 380);
+            diamond1[3] = new Point(45, 360);
+
+            Point[] diamond6 = new Point[4];
+            diamond1[0] = new Point(165, 340);
+            diamond1[1] = new Point(185, 360);
+            diamond1[2] = new Point(165, 380);
+            diamond1[3] = new Point(145, 360);
+
+            Point[] diamond7 = new Point[4];
+            diamond1[0] = new Point(265, 340);
+            diamond1[1] = new Point(285, 360);
+            diamond1[2] = new Point(265, 380);
+            diamond1[3] = new Point(245, 360);
+
+            Point[] diamond8 = new Point[4];
+            diamond1[0] = new Point(365, 340);
+            diamond1[1] = new Point(385, 360);
+            diamond1[2] = new Point(365, 380);
+            diamond1[3] = new Point(345, 360);
+
             g.Clear(Color.Red);
 
             g.DrawRectangle(drawPen, 30, 30, 100, 150);
             g.FillRectangle(whiteBrush, 30, 30, 100, 150);
+            g.FillPolygon(blackBrush, diamond1);
             g.DrawString("A", Cards, blackBrush, 35, 35);
             g.DrawString("A", Cards, blackBrush, 105, 155);
 
             g.DrawRectangle(drawPen, 210, 30, 100, 150);
             g.FillRectangle(whiteBrush, 210, 30, 100, 150);
+            g.FillPolygon(blackBrush, diamond2);
             g.DrawString("A", Cards, blackBrush, 215, 35);
             g.DrawString("A", Cards, blackBrush, 285, 155);
 
             g.DrawRectangle(drawPen, 390, 30, 100, 150);
             g.FillRectangle(whiteBrush, 390, 30, 100, 150);
+            g.FillPolygon(blackBrush, diamond3);
             g.DrawString("A", Cards, blackBrush, 395, 35);
             g.DrawString("A", Cards, blackBrush, 465, 155);
 
             g.DrawRectangle(drawPen, 570, 30, 100, 150);
             g.FillRectangle(whiteBrush, 570, 30, 100, 150);
+            g.FillPolygon(blackBrush, diamond4);
             g.DrawString("A", Cards, blackBrush, 575, 35);
             g.DrawString("A", Cards, blackBrush, 645, 155);
 
@@ -75,21 +126,25 @@ namespace GreetingCard._2
 
             g.DrawRectangle(drawPen, 30, 300, 100, 150);
             g.FillRectangle(whiteBrush, 30, 300, 100, 150);
+            g.FillPolygon(blackBrush, diamond5);
             g.DrawString("A", Cards, blackBrush, 35, 305);
             g.DrawString("A", Cards, blackBrush, 105, 425);
 
             g.DrawRectangle(drawPen, 210, 300, 100, 150);
             g.FillRectangle(whiteBrush, 210, 300, 100, 150);
+            g.FillPolygon(blackBrush, diamond6);
             g.DrawString("A", Cards, blackBrush, 215, 305);
             g.DrawString("A", Cards, blackBrush, 285, 425);
 
             g.DrawRectangle(drawPen, 390, 300, 100, 150);
             g.FillRectangle(whiteBrush, 390, 300, 100, 150);
+            g.FillPolygon(blackBrush, diamond7);
             g.DrawString("A", Cards, blackBrush, 395, 305);
             g.DrawString("A", Cards, blackBrush, 470, 425);
 
             g.DrawRectangle(drawPen, 570, 300, 100, 150);
             g.FillRectangle(whiteBrush, 570, 300, 100, 150);
+            g.FillPolygon(blackBrush, diamond8);
             g.DrawString("A", Cards, blackBrush, 575, 305);
             g.DrawString("A", Cards, blackBrush, 650, 425);
 
@@ -342,10 +397,60 @@ namespace GreetingCard._2
             g.DrawString("A", Large, blackBrush, 50, 50);
             g.DrawString("A", Large, blackBrush, 210, 325);
 
+
             g.DrawString("Was this Your Card?", Ariel, whiteBrush, 400, 230);
 
+            button1.Visible = true;
+            button2.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            button1.Visible = false;
+
+            Font drawFont = new Font("TimesNewRoman", 16, FontStyle.Bold);
+            SolidBrush whiteBrush = new SolidBrush(Color.White);
+            Graphics g = this.CreateGraphics();
+            g.Clear(Color.Black);
+
+            g.DrawString("Awesome. I knew it.", drawFont, whiteBrush, 400, 230);
+            g.DrawString("Play Again?", drawFont, whiteBrush, 400, 230);
+
+            //button3.Visible = true;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            button1.Visible = false;
+
+            Font drawFont = new Font("TimesNewRoman", 16, FontStyle.Bold);
+            SolidBrush whiteBrush = new SolidBrush(Color.White);
+            Graphics g = this.CreateGraphics();
+            g.Clear(Color.Black);
+
+            g.DrawString("Awesome. I knew it.", drawFont, whiteBrush, 400, 230);
+            g.DrawString("Play Again?", drawFont, whiteBrush, 400, 230);
+
+            //button3.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
             // Add "Awesome, I knew it."
             // And add a "Do you want to play again?" button
+
+            //The Following is code for when you press one of the 'yes' buttons
+
+            //button3.Visible = true;
+
+            //g.DrawString("Play Again?", timesNewRoman, whiteBrush, 400, 230);
         }
     }
 }
